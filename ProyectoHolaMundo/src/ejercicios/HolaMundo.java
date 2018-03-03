629f9762984ef5d4f15e2c7c6dd7f1a9c7f66178
Francisco Algaba <Francisco Algaba@DESKTOP-4NTHDBO>
2018-03-03 19:42:33 +0100

    Añade un bucle
    Este cambio agrega un bucle en la clase hola mundo, el bucle imprime 10
    veces por pantalla "Número: " y los números del 1 al 10.

--- ProyectoHolaMundo/src/ejercicios/HolaMundo.java
+++ ProyectoHolaMundo/src/ejercicios/HolaMundo.java
@@ -5,6 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Número: " + i);
+		}
 	}
 
 }
